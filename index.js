f70f8ae54884689033832d32b08b5348dde42252
siwagarnj <siwagarnj@gmail.com>
2023-03-26 00:33:20 +0700

    remove unused files

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const merge_1 = require("./merge");
-const collection_1 = [1, 2, 3];
-const collection_2 = [1, 2, 3, 9];
-console.log((0, merge_1.merge)(collection_1, collection_2));
